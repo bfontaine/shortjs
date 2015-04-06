2835de10b873a3600dab4f1b411a4426053f6861
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-06 22:34:13 +0200

    shorter Angular module prefix

--- bfn-short.js
+++ bfn-short.js
@@ -1,6 +1,6 @@
 // This is the Angular version of short.js.
 
-angular.module("bfontaine.short", []).
+angular.module("bfn.short", []).
   filter("short", function() {
 
     return function (text) { return short(text); };
